f12814bd9a6ae6c706ac8b28c63334843e43c12b
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-19 13:57:50 -0300

    add: e2e tests

---
+++ src/http/controllers/users/profile.spec.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Profile (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get user profile", async () => {
+    await request(app.server).post("/users").send({
+      name: "Jonh Doe",
+      email: "jonhdoe@example.com",
+      password: "test123",
+    });
+
+    const authResponse = await request(app.server).post("/sessions").send({
+      email: "jonhdoe@example.com",
+      password: "test123",
+    });
+
+    const { token } = authResponse.body;
+
+    const profileResponse = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(profileResponse.statusCode).toEqual(200);
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: "jonhdoe@example.com",
+      })
+    );
+  });
+});
