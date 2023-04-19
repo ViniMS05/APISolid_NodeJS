fd3f5eb8b72cd46c056fa988d3458726cd3eaeaa
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-19 13:31:27 -0300

    add: register e2e test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Jonh Doe",
+      email: "jonhdoe@example.com",
+      password: "test123",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
