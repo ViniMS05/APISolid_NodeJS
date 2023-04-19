4b3076ec529daa515403f63b2177fa167a32b187
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-19 12:21:43 -0300

    fit: profile controller

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,9 +1,17 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(req: FastifyRequest, res: FastifyReply) {
-  await req.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  console.log(req.user.sub);
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
 
-  return res.status(200).send();
+  return res.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
