3e41f4ecc475f4001e323532de2752266055aec3
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-19 12:22:15 -0300

    add: test environment

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { register } from "../controllers/register.controller";
 import { authenticate } from "../controllers/athenticate.controller";
 import { profile } from "../controllers/profile.controller";
+import { verifyJWT } from "../middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register); // Register user route
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post("/sessions", authenticate); // Authenticate user route
 
   // Authenticated
-  app.get("/me", profile); // User profile route
+  app.get("/me", { onRequest: [verifyJWT] }, profile); // User profile route
 }
