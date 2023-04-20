2dde61797e98e87cff681814434002b412f51182
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-20 13:14:54 -0300

    fix: nearby and search gyms controllers

--- src/http/controllers/gyms/nearby.controller.ts
+++ src/http/controllers/gyms/nearby.controller.ts
@@ -1,27 +1,27 @@
-import { z } from 'zod'
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
 
 export async function nearby(req: FastifyRequest, res: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
     latitude: z.number().refine((value) => {
-      return Math.abs(value) <= 90
+      return Math.abs(value) <= 90;
     }),
     longitude: z.number().refine((value) => {
-      return Math.abs(value) <= 180
+      return Math.abs(value) <= 180;
     }),
-  })
+  });
 
-  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.body)
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.query);
 
-  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase();
 
   const { gyms } = await fetchNearbyGymsUseCase.execute({
     userLatitude: latitude,
     userLongitude: longitude,
-  })
+  });
 
   return res.status(200).send({
     gyms,
-  })
+  });
 }
