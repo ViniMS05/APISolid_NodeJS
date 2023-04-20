6cb3de5704b4c8995a64435b355066cd652e47e2
Vinicius Silva <vmsilva1227@gmail.com>
2023-04-20 13:29:02 -0300

    add: get user metrics controller

---
+++ src/http/controllers/check-ins/metrics.controller.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+
+export async function metrics(req: FastifyRequest, res: FastifyReply) {
+  const getUserMetricsUseCase = makeGetUserMetricsUseCase()
+
+  const { checkInsCount } = await getUserMetricsUseCase.execute({
+    userId: req.user.sub,
+  })
+
+  return res.status(200).send({
+    checkInsCount,
+  })
+}
